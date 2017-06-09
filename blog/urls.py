a524b20faaa5f55dd08e63cafe30fa1631fa3997
Luyten44 <laugir.s.d@gmail.com>
2017-06-09 12:50:52 +1000

    all done

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     	url(r'^$', views.post_list, name='post_list'),
     	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 	url(r'^post/new/$', views.post_new, name='post_new'),
+	url(r'^json_all_posts$', views.json_all_posts, name='json_all_posts'),
+	url(r'^spa_post_list$', views.spa_post_list, name='spa_post_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.http import JsonResponse
+from django.core import serializers
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -42,3 +44,12 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def json_all_posts(request):
+    posts = Post.objects.order_by('published_date')
+    data = serializers.serialize('json', posts)
+    return JsonResponse(data, safe=False)
+
+def spa_post_list(request):
+    return render(request, 'blog/spa_post_list.html', {})
+
